674ed2ae3375c7dad73a9301af9f3e01653ea114
Seyacat <seyacat@gmail.com>
2024-03-31 20:43:20 -0500

    Pricetable fix

--- formwt.js
+++ formwt.js
@@ -160,7 +160,7 @@ const nextFormWT = (event) => {
 const createTrip = async (event) => {
   const formData = getFormData();
 
-  unitPrice = pricetable[pricetable.length - 1];
+  unitPrice = Object.values(pricetable)[Object.values(pricetable).length - 1];
 
   if (pricetable[formData.Total_Number_Pax__c]) {
     unitPrice = pricetable[formData.Total_Number_Pax__c];
